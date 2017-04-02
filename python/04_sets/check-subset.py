612ac2706c389f6df50b4fbe18d89f29b1f3515c
Shakil <sakil58@gmail.com>
2017-04-02 23:37:47 +0600

    error fixes

--- python/04_sets/check-subset.py
+++ python/04_sets/check-subset.py
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/py-check-subset
 
-for i in range(int(input())): #More than 4 lines will result in 0 score. Blank lines won't be counted. 
+for i in range(int(input())): 
     a = int(input()); A = set(input().split()) 
     b = int(input()); B = set(input().split())
     print(A & B == A)
